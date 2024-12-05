cfe0eb7d6f154b25ea53cb6218bde72efbef8c93
Izaias Francisco <izaiasfrancisco2020@gmail.com>
2024-12-05 10:58:38 -0300

    Primeiro commit - projeto React

---
+++ src/components/Frase.js
@@ -0,0 +1,46 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {useState, useEffect} from 'react'
+import { useTranslation } from 'react-i18next';
+
+function Frase(){
+    const {t, i18n} = useTranslation()
+    const [frases, setFrases] = useState([])
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    useEffect(() => {
+        const fetchFrases = async () => {
+          const fetchedFrases = await t('frases', { returnObjects: true });
+          setFrases(fetchedFrases);
+        };
+        fetchFrases();
+    }, [i18n.language, t]);
+  
+    useEffect(() => {
+        if (frases.length > 0) {
+          setFrase(frases[currentIndex].texto);
+          setAutor(frases[currentIndex].autor);
+        }
+      }, [currentIndex, frases]);
+
+      const [frase, setFrase] = useState('');
+      const [autor, setAutor] = useState('');
+ 
+  function Clicar(){
+    
+      const newIndex = (currentIndex + 1) % frase.length;
+     
+      setCurrentIndex(newIndex)
+  }
+ 
+    return (
+     <div className="frase">   
+      <span>"{frase}"</span>
+      <h4>{autor}</h4>
+      <FontAwesomeIcon className="icone" onClick={Clicar} icon={faSpinner} />
+     </div>
+    )
+}
+
+export default Frase;
---
+++ src/components/Hora.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { getCurrentTime } from './CurrentTime';
+import { useTranslation } from 'react-i18next';
+
+function Hora(){
+     const {t} = useTranslation()
+     const [hora, setHora] = useState(getCurrentTime());
+     
+     useEffect(() => {
+        const intervalo = setInterval(() => {
+           const currentTime = getCurrentTime();
+           setHora(currentTime);
+        }, 1000)
+        return () => clearInterval(intervalo)
+     }, [])
+
+    return(  
+    <div className="hora">
+
+      <h2>{t('hora.greeting')}<FontAwesomeIcon className="icone_moon" icon={faMoon} /></h2>
+      <h1>{hora}</h1>
+      <h3>{t('hora.local')}</h3> 
+
+    </div>
+    )
+}
+
+export default Hora;
